4bad02cf542cce32129f9d2b86b386702f4b152f
Levent SEFLEK <leventseflek@gmail.com>
2024-08-12 06:25:51 -0400

    levent v2

--- src/Ortak.java
+++ src/Ortak.java
@@ -1,5 +1,7 @@
 public class Ortak {
     public static void main(String[] args) {
         System.out.println("Ne yaparsan Yap AŞK ile Yap!");
+
+        System.out.println("Cezerye Guzeldir");
     }
 }
